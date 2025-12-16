05ccf3375a2faa4b9efd3fe2e1e509fbda02a15e
m2k3a <68491322+m2k3a@users.noreply.github.com>
2025-12-16 13:14:45 +0100

    Update allanime.js

--- javascript/anime/src/en/allanime.js
+++ javascript/anime/src/en/allanime.js
@@ -7,7 +7,7 @@ const mangayomiSources = [{
     "typeSource": "single",
     "itemType": 1,
     "isNsfw": false,
-    "version": "0.0.35",
+    "version": "0.0.4",
     "dateFormat": "",
     "dateFormatLocale": "",
     "pkgPath": "anime/src/en/allanime.js"

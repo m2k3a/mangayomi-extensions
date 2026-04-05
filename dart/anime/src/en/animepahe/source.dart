4ea480ace0f5771766b0f18cb2d37977ac018ed2
XxeylakxX <99536219+XxeylakxX@users.noreply.github.com>
2026-04-05 23:28:53 +0800

    Update source.dart

--- dart/anime/src/en/animepahe/source.dart
+++ dart/anime/src/en/animepahe/source.dart
@@ -3,7 +3,7 @@ import '../../../../../model/source.dart';
 Source get animepaheSource => _animepaheSource;
 const _animepaheVersion = "0.0.85";
 const _animepaheSourceCodeUrl =
-    "https://raw.githubusercontent.com/m2k3a/mangayomi-extensions/$branchName/dart/anime/src/en/animepahe/animepahe.dart";
+    "https://raw.githubusercontent.com/XxeylakxX/mangayomi-extensions/$branchName/dart/anime/src/en/animepahe/animepahe.dart";
 Source _animepaheSource = Source(
   name: "AnimePahe",
   baseUrl: "https://www.animepahe.ru",
